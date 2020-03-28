097bf823214cd0fcf3ed8a7efb2b5d581a2bb87b
David-Matos-Salvador <brok504brok504@gmail.com>
2020-03-27 19:54:34 -0500

    Updates

---
+++ precache-manifest.6a1b9b6dfb69167c3f8b0548317b21ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cd46240d821834dce86f4894d136576",
+    "url": "/Calculadora1/index.html"
+  },
+  {
+    "revision": "dc2a4b67a4d16f9f1c84",
+    "url": "/Calculadora1/static/css/main.b792217a.chunk.css"
+  },
+  {
+    "revision": "a341de22a2353e56103a",
+    "url": "/Calculadora1/static/js/2.9a46b7a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculadora1/static/js/2.9a46b7a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc2a4b67a4d16f9f1c84",
+    "url": "/Calculadora1/static/js/main.8c1b1798.chunk.js"
+  },
+  {
+    "revision": "a2272cc5da7597a08983",
+    "url": "/Calculadora1/static/js/runtime-main.548407c4.js"
+  },
+  {
+    "revision": "6fb6f274ad815925815b97fd69282de5",
+    "url": "/Calculadora1/static/media/6f5c58cbdb45d470fb21054337bbe0a4.6fb6f274.gif"
+  }
+]);
